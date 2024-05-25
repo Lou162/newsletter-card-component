86a2cb3621de6ffecd84449b0a31d34d08c50db1
lou162 <louna.libbrecht@gmail.com>
2024-05-25 21:16:33 +0200

    ajout vérification mail

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
 // write your JavaScript here
+function get_email() {
+  mailAdresse = document.getElementById("email").value;
+  if (
+    mailAdresse.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  ) {
+    document.getElementById("email_verif").innerHTML =
+      mailAdresse + " est correct";
+  }
+  // sign_bool = document.getElementsByClassName();
+  console.log(mailAdresse);
+}
